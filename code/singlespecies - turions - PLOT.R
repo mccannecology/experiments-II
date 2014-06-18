d00547dbab0dfe84e76049b1f671cdc6db7806d8
mccannecology <mccannmikejames@gmail.com>
2014-06-18 16:19:49 -0400

    fixing up some plots

--- code/singlespecies - turions - PLOT.R
+++ code/singlespecies - turions - PLOT.R
@@ -52,10 +52,11 @@ ggsave(filename = "mean_summary_data_turions_plot.jpg", mean_summary_data_turion
 
 
 
-##############
-# turionsTOT #
-# Average    #
-##############
+################
+# turionsTOT   #
+# Average      #
+# Wolffia only #
+################
 # subset - just WB data 
 summary_data_turion_area_per_day_WB <- subset(summary_data_turion_area_per_day, summary_data_turion_area_per_day$species=="WB")
 
@@ -86,6 +87,46 @@ turion_area_per_day_plot <- turion_area_per_day_plot + ylab(expression(paste("Tu
 turion_area_per_day_plot <- turion_area_per_day_plot + xlab("Species")
 turion_area_per_day_plot <- turion_area_per_day_plot + theme_bw(base_size=18)
 turion_area_per_day_plot <- turion_area_per_day_plot + geom_text(data=summary_data_turion_area_per_day_WB,aes(x=species, y=turion_area_per_day+se+0.35,label=label))
+turion_area_per_day_plot 
+
+# save it 
+ggsave(filename = "turion_area_per_day_plot.jpg", turion_area_per_day_plot, height=11, width=11)
+
+
+
+
+################
+# turionsTOT   #
+# Average      #
+# all species  #
+################
+# import the post-hoc test labels 
+summary_data_turion_area_per_day <- read.csv("turions_area_per_day_posthoc.csv")
+summary_data_turion_area_per_day$Nutr <- factor(summary_data_turion_area_per_day$Nutr , levels=c("low","med","high"))
+summary_data_turion_area_per_day$Temp <- as.factor(summary_data_turion_area_per_day$Temp)
+summary_data_turion_area_per_day$Temp <- factor(summary_data_turion_area_per_day$Temp , levels=c("18","24","30"))
+
+# labelling the facet variables
+nutrient_names <- list("low"="Low nutrients","med"="Medium nutrients","high"="High nutrients")
+temperature_names <- list("18"=expression(paste("18 ",degree,"C")),"24"=expression(paste("24 ",degree,"C")),"30"=expression(paste("30 ",degree,"C")))
+
+labeller_function <- function(variable,value){
+  if (variable=="Temp") {
+    return(temperature_names[value])
+  } else {
+    return(nutrient_names[value])
+  }
+}
+
+# making the plot 
+turion_area_per_day_plot <- ggplot(summary_data_turion_area_per_day, aes(x=species,y=turion_area_per_day)) 
+turion_area_per_day_plot <- turion_area_per_day_plot + geom_point() 
+turion_area_per_day_plot <- turion_area_per_day_plot + geom_errorbar(aes(ymin=turion_area_per_day-se, ymax=turion_area_per_day+se), width=0.1)
+turion_area_per_day_plot <- turion_area_per_day_plot + facet_grid(Temp ~ Nutr, labeller=labeller_function)
+turion_area_per_day_plot <- turion_area_per_day_plot + ylab(expression(paste("Turion production (", mm^2,day^-1,")",sep="")))
+turion_area_per_day_plot <- turion_area_per_day_plot + xlab("Species")
+turion_area_per_day_plot <- turion_area_per_day_plot + theme_bw(base_size=18)
+turion_area_per_day_plot <- turion_area_per_day_plot + geom_text(data=summary_data_turion_area_per_day,aes(x=species, y=turion_area_per_day+se+0.35,label=label))
 turion_area_per_day_plot 
 
 # save it 
